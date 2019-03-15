66f35815b6a10fa803f9641bce45d06880454c0e
piotrgorow <piotrg2@tlen.pl>
2019-03-15 17:56:32 +0100

    Task #6 First Pull Request - class SecondPullRequest was added

---
+++ src/main/java/pl/coderstrust/first_branch/SecondPullRequest.java
@@ -0,0 +1,8 @@
+package pl.coderstrust.first_branch;
+
+public class SecondPullRequest {
+
+    public static void main(String[] args) {
+        System.out.println("Second Pull Request");
+    }
+}
