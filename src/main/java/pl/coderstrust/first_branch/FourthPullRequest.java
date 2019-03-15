4d1505d6904de7af3e0f94656f73cb6febfd1f53
piotrgorow <piotrg2@tlen.pl>
2019-03-15 18:00:25 +0100

    Task #6 First Pull Request - class FourthPullRequest was added

---
+++ src/main/java/pl/coderstrust/first_branch/FourthPullRequest.java
@@ -0,0 +1,8 @@
+package pl.coderstrust.first_branch;
+
+public class FourthPullRequest {
+
+    public static void main(String[] args) {
+        System.out.println("Fourth Pull Request");
+    }
+}
