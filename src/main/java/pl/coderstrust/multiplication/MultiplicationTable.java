a4c92c047608a9a37ea97e13d450673ab9f50cc4
piotrgorow <piotrg2@tlen.pl>
2019-03-24 19:59:25 +0100

    Task #11 Multiplication Table tests - return parameter type list was changed to array[][] - test was adjusted to new parameter

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -1,43 +1,23 @@
 package pl.coderstrust.multiplication;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class MultiplicationTable {
 
     public static void main(String[] args) {
-        getMultiplicationTable(12).forEach(System.out::println);
+        System.out.println(Arrays.deepToString(getMultiplicationTable(12)));
     }
 
-    public static List<String> getMultiplicationTable(int size) {
+    public static int[][] getMultiplicationTable(int size) {
         if (size < 1) {
             throw new IllegalArgumentException("Size cannot be lower than zero.");
         }
-        StringBuilder line = new StringBuilder();
-        List<String> result = new ArrayList<>();
-        line.append("     ");
+        int[][] result = new int[size][size];
         for (int i = 1; i <= size; i++) {
-            line.append(getNumberInField(i, 5));
-        }
-        result.add(line.toString());
-        line.delete(0, line.length());
-        for (int i = 1; i <= size; i++) {
-            line.append(getNumberInField(i, 5));
             for (int j = 1; j <= size; j++) {
-                line.append(getNumberInField(i * j, 5));
+                result[j - 1][i - 1] = i * j;
             }
-            result.add(line.toString());
-            line.delete(0, line.length());
         }
-        return result;
-    }
-
-    private static StringBuilder getNumberInField(int number, int width) {
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < width - String.valueOf(number).length(); i++) {
-            result.append(" ");
-        }
-        result.append(number);
         return result;
     }
 }
