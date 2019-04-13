e8280050d97f00fb70bddcf31142b1a95f0a10b0
Paweł Zięba <zieba.pawel@gmail.com>
2019-04-13 10:27:56 +0200

    Update src/test/java/pl/coderstrust/regex/RegexTest.java
    Co-Authored-By: piotrgorow <47689484+piotrgorow@users.noreply.github.com>

--- src/test/java/pl/coderstrust/regex/RegexTest.java
+++ src/test/java/pl/coderstrust/regex/RegexTest.java
@@ -10,7 +10,7 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class RegexTest {
+class IpAddressValidatorTest {
 
     @ParameterizedTest
     @MethodSource("parameters")
