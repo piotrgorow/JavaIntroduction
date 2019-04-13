8878e25c4769d961fb098fefc493f986bd013489
piotrgorow <piotrg2@tlen.pl>
2019-04-13 11:37:48 +0200

    Task #24 Regex - constructor was removed - exception test for null string was added - test for invalid and valid IP addresses was created

--- src/test/java/pl/coderstrust/ipadress/IpAddressValidatorTest.java
+++ src/test/java/pl/coderstrust/ipadress/IpAddressValidatorTest.java
@@ -8,41 +8,58 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class IpAddressValidatorTest {
 
     @ParameterizedTest
-    @MethodSource("parameters")
-    public void shouldVerifyIfStringIsProperIPAddress(String given, boolean expected) {
-        //given
-        Regex regex = new Regex();
-
+    @MethodSource("invalidTestArguments")
+    public void shouldReturnFalseForInvalidIpAddresses(String given, boolean expected) {
         //when
-        boolean result = regex.isIpAddress(given);
+        boolean result = IpAddressValidator.isIpAddress(given);
 
         //then
         assertEquals(expected, result);
     }
 
-    private static Stream<Arguments> parameters() {
+    private static Stream<Arguments> invalidTestArguments() {
         return Stream.of(
-                Arguments.of("192.168.0.1", true),
                 Arguments.of("192.168.0", false),
-                Arguments.of("192.168.000.000", true),
-                Arguments.of("1.1.0.0", true),
-                Arguments.of("001.001.000.000", true),
                 Arguments.of("192.168.0.256", false),
                 Arguments.of("192.168.256.1", false),
                 Arguments.of("192.256.0.1", false),
                 Arguments.of("256.168.0.1", false),
-                Arguments.of("0.10.100.200", true),
-                Arguments.of("249.250.255.99", true),
-                Arguments.of("255.168.00.255", true),
                 Arguments.of("000..0.256", false),
                 Arguments.of("019.01.0.1.", false),
                 Arguments.of(".019.01.0.1", false)
         );
+    }
+
+    @ParameterizedTest
+    @MethodSource("validTestArguments")
+    public void shouldReturnTrueForValidIpAddresses(String given, boolean expected) {
+        //when
+        boolean result = IpAddressValidator.isIpAddress(given);
+
+        //then
+        assertEquals(expected, result);
+    }
+
+    private static Stream<Arguments> validTestArguments() {
+        return Stream.of(
+                Arguments.of("192.168.0.1", true),
+                Arguments.of("192.168.000.000", true),
+                Arguments.of("1.1.0.0", true),
+                Arguments.of("001.001.000.000", true),
+                Arguments.of("0.10.100.200", true),
+                Arguments.of("249.250.255.99", true),
+                Arguments.of("255.168.00.255", true)
+        );
+    }
+
+    @Test
+    public void shouldThrowExceptionForNullArgument() {
+        assertThrows(IllegalArgumentException.class, () -> IpAddressValidator.isIpAddress(null));
     }
 
     @Test
@@ -50,22 +67,14 @@ class IpAddressValidatorTest {
     public void shouldReturnTrueForAllCombinationsOfIpAddress() {
         String ipAddress;
         for (int i = 0; i < 256; i++) {
-            System.out.print(i + " ");
             for (int j = 0; j < 256; j++) {
-                if (j % 10 == 0) {
-                    System.out.print(".");
-                }
                 for (int k = 0; k < 256; k++) {
                     for (int l = 0; l < 256; l++) {
                         ipAddress = String.format("%d.%d.%d.%d", i, j, k, l);
-                        Assert.assertTrue(IpAddressValidator.isIpAddress(ipAddress));
-                            System.out.println();
-                            System.out.println("Error on address: " + i + "." + j + "." + k + "." + l);
-                        }
+                        assertTrue(IpAddressValidator.isIpAddress(ipAddress));
                     }
                 }
             }
-            System.out.println();
         }
     }
 }
