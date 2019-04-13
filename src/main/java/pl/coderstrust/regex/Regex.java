7f913a570a812809490ce9f6362582f2b5255b20
Paweł Zięba <zieba.pawel@gmail.com>
2019-04-13 10:24:36 +0200

    Update src/main/java/pl/coderstrust/regex/Regex.java
    Co-Authored-By: piotrgorow <47689484+piotrgorow@users.noreply.github.com>

--- src/main/java/pl/coderstrust/regex/Regex.java
+++ src/main/java/pl/coderstrust/regex/Regex.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class IpAddressValidator {
-    private Pattern pattern;
+    private static Pattern pattern = Pattern.compile("(([0-9]|[0-1]?[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[0-1]?[0-9][0-9]|2[0-4][0-9]|25[0-5])");
 
     public Regex() {
         pattern = Pattern.compile("(([0-9]|[0-1]?[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[0-1]?[0-9][0-9]|2[0-4][0-9]|25[0-5])");
