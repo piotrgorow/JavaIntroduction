3ea38b8c60ce0eeedab9cc8b9d83314398b3632f
Paweł Zięba <zieba.pawel@gmail.com>
2019-04-17 17:26:03 +0200

    Update src/main/java/pl/coderstrust/fibonacci/FibonacciChecker.java
    Co-Authored-By: piotrgorow <47689484+piotrgorow@users.noreply.github.com>

--- src/main/java/pl/coderstrust/fibonacci/FibonacciChecker.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciChecker.java
@@ -6,7 +6,9 @@ public class FibonacciChecker {
         if (number < 1) {
             throw new IllegalArgumentException("Number cannot be less then zero.");
         }
-        return isPerfectSquare(5 * number * number + 4) || isPerfectSquare(5 * number * number - 4);
+        long formula1 = 5 * (number * number) + 4;
+        long formula2 = 5 * (number * number) + 4;
+        return isPerfectSquare(formula1 ) || isPerfectSquare(formula2);
     }
 
     private static boolean isPerfectSquare(long number) {
