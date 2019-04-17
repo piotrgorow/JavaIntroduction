e3727881d238d7258a8ef92e7beba6954776684b
piotrgorow <piotrg2@tlen.pl>
2019-04-17 18:37:29 +0200

    Task #17 IsFibonacciNumber - HashMap was added

--- src/main/java/pl/coderstrust/fibonacci/FibonacciChecker.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciChecker.java
@@ -1,18 +1,27 @@
 package pl.coderstrust.fibonacci;
 
+import java.util.HashMap;
+
 public class FibonacciChecker {
 
-    boolean isFibonacciNumber(long number) {
+    private static HashMap<Long, Boolean> hashMap = new HashMap<>();
+
+    public static boolean isFibonacciNumber(long number) {
         if (number < 1) {
             throw new IllegalArgumentException("Number cannot be less then zero.");
         }
-        long formula1 = 5 * (number * number) + 4;
+        if (hashMap.containsKey(number)) {
+            return hashMap.get(number);
+        }
+        long formula1 = 5 * (number * number) - 4;
         long formula2 = 5 * (number * number) + 4;
-        return isPerfectSquare(formula1 ) || isPerfectSquare(formula2);
+        boolean result = isPerfectSquare(formula1) || isPerfectSquare(formula2);
+        hashMap.put(number, result);
+        return result;
     }
 
     private static boolean isPerfectSquare(long number) {
         long squareRoot = (long) Math.sqrt(number);
-        return (s * s == number);
+        return (squareRoot * squareRoot == number);
     }
 }
--- src/test/java/pl/coderstrust/fibonacci/FibonacciCheckerTest.java
+++ src/test/java/pl/coderstrust/fibonacci/FibonacciCheckerTest.java
@@ -8,25 +8,24 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FibonacciCheckerTest {
-    private FibonacciChecker fibonacciChecker = new FibonacciChecker();
 
     @ParameterizedTest
     @ValueSource(longs = {1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946,
             17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269})
     void shouldReturnTrueForFibonacciNumber(long number) {
-        assertTrue(fibonacciChecker.isFibonacciNumber(number));
+        assertTrue(FibonacciChecker.isFibonacciNumber(number));
     }
 
     @ParameterizedTest
     @ValueSource(longs = {4, 6, 10, 15, 25, 35, 50, 80, 145, 232, 378, 611, 988, 1598, 2585, 4182, 6760, 10945,
             17712, 28658, 46360, 75020, 121390, 196419, 317812, 514220, 832041, 1346260})
     void shouldReturnFalseForNonFibonacciNumber(long number) {
-        assertFalse(fibonacciChecker.isFibonacciNumber(number));
+        assertFalse(FibonacciChecker.isFibonacciNumber(number));
     }
 
     @ParameterizedTest
     @ValueSource(longs = {0, -1, -2, -10, -50, -100, -1000})
     void shouldThrowExceptionForInvalidNumber(long number) {
-        assertThrows(IllegalArgumentException.class, () -> fibonacciChecker.isFibonacciNumber(number));
+        assertThrows(IllegalArgumentException.class, () -> FibonacciChecker.isFibonacciNumber(number));
     }
 }
