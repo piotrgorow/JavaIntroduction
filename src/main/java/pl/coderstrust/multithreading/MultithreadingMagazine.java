1c4bd328b1c2c1b058f0f3016eca66beca4311f4
piotrgorow <piotrg2@tlen.pl>
2019-04-28 19:11:39 +0200

    Task #27 Multithreading magazine - threadPool was created

--- src/main/java/pl/coderstrust/multithreading/MultithreadingMagazine.java
+++ src/main/java/pl/coderstrust/multithreading/MultithreadingMagazine.java
@@ -2,6 +2,8 @@ package pl.coderstrust.multithreading;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MultithreadingMagazine {
@@ -14,29 +16,32 @@ public class MultithreadingMagazine {
     public void oneProducerAndOneConsumer() {
         BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10);
         AtomicInteger counter = new AtomicInteger();
-        new Thread(new Producer(queue, 500, "Producer-1", counter)).start();
-        new Thread(new Consumer(queue, 500, "Consumer-1")).start();
+        ExecutorService threadPool = Executors.newFixedThreadPool(2);
+        threadPool.submit(new Producer(queue, 500, "Producer-1", counter));
+        threadPool.submit(new Consumer(queue, 500, "Consumer-1"));
     }
 
     public void fiveProducersAndOneConsumer() {
         BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10);
         AtomicInteger counter = new AtomicInteger();
-        new Thread(new Producer(queue, 500, "Producer-1", counter)).start();
-        new Thread(new Producer(queue, 500, "Producer-2", counter)).start();
-        new Thread(new Producer(queue, 500, "Producer-3", counter)).start();
-        new Thread(new Producer(queue, 500, "Producer-4", counter)).start();
-        new Thread(new Producer(queue, 500, "Producer-5", counter)).start();
-        new Thread(new Consumer(queue, 500, "Consumer-1")).start();
+        ExecutorService threadPool = Executors.newFixedThreadPool(6);
+        threadPool.submit(new Producer(queue, 500, "Producer-1", counter));
+        threadPool.submit(new Producer(queue, 500, "Producer-2", counter));
+        threadPool.submit(new Producer(queue, 500, "Producer-3", counter));
+        threadPool.submit(new Producer(queue, 500, "Producer-4", counter));
+        threadPool.submit(new Producer(queue, 500, "Producer-5", counter));
+        threadPool.submit(new Consumer(queue, 500, "Consumer-1"));
     }
 
     public void oneProducerAndFiveConsumers() {
         BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10);
         AtomicInteger counter = new AtomicInteger();
-        new Thread(new Producer(queue, 500, "Producer-1", counter)).start();
-        new Thread(new Consumer(queue, 500, "Consumer-1")).start();
-        new Thread(new Consumer(queue, 500, "Consumer-2")).start();
-        new Thread(new Consumer(queue, 500, "Consumer-3")).start();
-        new Thread(new Consumer(queue, 500, "Consumer-4")).start();
-        new Thread(new Consumer(queue, 500, "Consumer-5")).start();
+        ExecutorService threadPool = Executors.newFixedThreadPool(6);
+        threadPool.submit(new Producer(queue, 500, "Producer-1", counter));
+        threadPool.submit(new Consumer(queue, 500, "Consumer-1"));
+        threadPool.submit(new Consumer(queue, 500, "Consumer-2"));
+        threadPool.submit(new Consumer(queue, 500, "Consumer-3"));
+        threadPool.submit(new Consumer(queue, 500, "Consumer-4"));
+        threadPool.submit(new Consumer(queue, 500, "Consumer-5"));
     }
 }
