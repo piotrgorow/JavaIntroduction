776b1160f0b5a718681b6a7aaa1fe0975e23d1e7
piotrgorow <piotrg2@tlen.pl>
2019-05-06 16:27:30 +0200

    Task #15 Hanoi Towers was created

---
+++ src/main/java/pl/coderstrust/hanoi/HanoiTowers.java
@@ -0,0 +1,38 @@
+package pl.coderstrust.hanoi;
+
+import java.util.Stack;
+
+public class HanoiTowers {
+    private static Stack<Integer> A = new Stack<>();
+    private static Stack<Integer> B = new Stack<>();
+    private static Stack<Integer> C = new Stack<>();
+
+    public static void doTowers(int topN, Stack<Integer> from, Stack<Integer> inter, Stack<Integer> to, char labelFrom, char labelInter, char labelTo) {
+        if (topN == 1) {
+            System.out.println("Moving the disk 1 from " + labelFrom + " to " + labelTo);
+            to.push(from.pop());
+            printTowers();
+        } else {
+            doTowers(topN - 1, from, to, inter, labelFrom, labelTo, labelInter);
+            System.out.println("Moving the disk " + topN + " from " + labelFrom + " to " + labelTo);
+            to.push(from.pop());
+            printTowers();
+            doTowers(topN - 1, inter, from, to, labelInter, labelFrom, labelTo);
+        }
+    }
+
+    private static void printTowers() {
+        System.out.println("A " + A);
+        System.out.println("B " + B);
+        System.out.println("C " + C);
+    }
+
+    public static void main(String[] args) {
+        int nDisks = 3;
+        for (int i = nDisks; i > 0; i--) {
+            A.push(i);
+        }
+        printTowers();
+        doTowers(nDisks, A, B, C, 'A', 'B', 'C');
+    }
+}
