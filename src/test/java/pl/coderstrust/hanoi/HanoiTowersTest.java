3b93ae638ff2c5fc8e0b4bf50ec88d8bb2c2364c
piotrgorow <47689484+piotrgorow@users.noreply.github.com>
2019-05-14 06:41:12 +0200

    Update src/test/java/pl/coderstrust/hanoi/HanoiTowersTest.java
    Co-Authored-By: Paweł Zięba <zieba.pawel@gmail.com>

--- src/test/java/pl/coderstrust/hanoi/HanoiTowersTest.java
+++ src/test/java/pl/coderstrust/hanoi/HanoiTowersTest.java
@@ -24,7 +24,13 @@ class HanoiTowersTest {
         towers[0].push(2);
         towers[0].push(1);
         HanoiTowers hanoiTowers = new HanoiTowers(towers);
-        Stack<Integer>[] expected = new Stack[]{new Stack<Integer>(), new Stack<Integer>(), new Stack<Integer>()};
+        Stack<Integer>[] expected = new Stack[3];
+        expected[0] = new Stack<>();
+        expected[1] = new Stack<>();
+        expected[2] = new Stack<>();
+        expected[2].push(3);
+        expected[2].push(2);
+        expected[2].push(1);
         expected[2].push(4);
         expected[2].push(3);
         expected[2].push(2);
